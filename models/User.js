937641aea2e210440a6e5e43a29afcafdc9a53ae
duc0905 <34796126+duc0905@users.noreply.github.com>
2020-03-31 00:31:16 +0700

    rename model files
    Org.js and User.js

--- models/User.js
+++ models/User.js
@@ -1,9 +1,12 @@
 const mongoose = require('mongoose');
+const Scheme = mongoose.Schema;
 
-const userSchema = new mongoose.Schema({
-  name: String,
-  email: String,
-  phoneNumber: String
+const userSchema = new Schema({
+    id: mongoose.Schema.Types.ObjectId,
+    name: String,
+    dob: String,
+    ava: String,
+    bio: String,
 });
 
 module.exports = mongoose.model('User', userSchema);
